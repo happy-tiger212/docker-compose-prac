02a17699165271e9bff780b0dcc9fa0ad1c93498
seoyun <dhn04100@gmail.com>
2022-08-22 18:43:05 +0900

    ADD: ip주소 반환하도록 구현

--- doc_prac/greeting/models.py
+++ doc_prac/greeting/models.py
@@ -2,6 +2,8 @@ from django.db import models
 
 class User(models.Model):
     name = models.CharField(max_length=50)
-
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    ip_address = models.GenericIPAddressField(null=True)
     class Meta:
         db_table = "users"
--- doc_prac/greeting/views.py
+++ doc_prac/greeting/views.py
@@ -6,7 +6,33 @@ from .models import User
 
 class GreetingView(View):
     def post(self, request):
-        data = json.loads(request.body)
-        name = data['name']
-        User.objects.create(name = name)
-        return JsonResponse({'message': 'Hello, ' + name}, status=201)
+        try:
+            data            = json.loads(request.body)
+            x_forwarded_for = request.headers.get('HTTP_X_FORWARDED_FOR')
+
+            name = data['name']
+
+            if x_forwarded_for:
+                # 여기서 왜 맨 마지막껄하지>? 맨앞이 클라이언트인데?
+                ip = x_forwarded_for.split(',')[-1].strip()
+            else:
+                # REMOTE_ADDR이 뭐지?
+                ip = request.META.get('REMOTE_ADDR')
+                print(ip)
+
+            if not User.objects.filter(name=name).exists():
+                User.objects.create(name = name, ip_address = ip)
+            else:
+                [i.save() for i in User.objects.filter(name=name)]
+            
+            result = [{
+                'name' : user.name,
+                'created_at' : user.created_at.strftime('%Y-%m-%d %H:%M:%S'),
+                'updated_at': user.updated_at.strftime('%Y-%m-%d %H:%M:%S'),
+                'ip': user.ip_address
+            }for user in User.objects.all()]
+            
+            return JsonResponse({"result": result}, status=200)
+
+        except:
+            return JsonResponse({"message" : "ERROR"}, status=400)
